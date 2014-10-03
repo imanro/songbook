c406c4a1c3170dc3f8dc2e6d49dd7db316668078
manro <manro@manro-softheme>
2014-10-03 18:12:43 +0300

    modules added

--- config/application.config.php
+++ config/application.config.php
@@ -2,8 +2,11 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
+        'ZendDeveloperTools',
+        'DoctrineModule',
+        'DoctrineORMModule',
         'Application',
-    	  'Songbook',
+        'Songbook'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
