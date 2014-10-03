56ecb1fb8f378c3a9f62f9bfd0bba149d6e41847
manro <manro@manro-softheme>
2014-10-03 17:22:02 +0300

    added

--- module/Songbook/config/module.config.php
+++ module/Songbook/config/module.config.php
@@ -1,15 +1,34 @@
 <?php
-
 return array(
     'controllers' => array(
         'invokables' => array(
-            'Songbook\Controller\Songbook' => 'Songbook\Controller\SongbookController',
-        ),
+            'Songbook\Controller\Song' => 'Songbook\Controller\SongController'
+        )
+    ),
+
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'song' => array(
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/song[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+'
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Songbook\Controller\Song',
+                        'action' => 'index'
+                    )
+                )
+            )
+        )
     ),
     'view_manager' => array(
         'template_path_stack' => array(
-            'songbook' => __DIR__ . '/../view',
-        ),
-    ),
+            'songbook' => __DIR__ . '/../view'
+        )
+    )
 );
 
---
+++ module/Songbook/src/Songbook/Controller/Song.php
@@ -0,0 +1,20 @@
+<?php
+namespace Song\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class SongController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function viewAction()
+    {
+    }
+}
